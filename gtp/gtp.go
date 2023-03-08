84323cc7cc204771026fe9385c9f5e19b43a91ce
superheze <heze325@gmail.com>
2023-03-08 20:00:39 +0800

    代码接入chatgpt3.5版本，模型：gpt-3.5-turbo

---
+++ gtp/gtp.go
@@ -0,0 +1,103 @@
+package gtp
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/869413421/wechatbot/config"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const BASEURL = "https://api.openai.com/v1/chat/"
+
+// ChatGPTResponseBody 请求体
+type ChatGPTResponseBody struct {
+	ID      string                 `json:"id"`
+	Object  string                 `json:"object"`
+	Created int                    `json:"created"`
+	Model   string                 `json:"model"`
+	Choices []ChoiceItem           `json:"choices"`
+	Usage   map[string]interface{} `json:"usage"`
+}
+
+type ChoiceItem struct {
+	Index        int      `json:"index"`
+	FinishReason string   `json:"finish_reason"`
+	Msg          messages `json:"message"`
+}
+
+// ChatGPTRequestBody 响应体
+type ChatGPTRequestBody struct {
+	Model            string     `json:"model"`
+	Prompt           []messages `json:"messages"`
+	MaxTokens        int        `json:"max_tokens"`
+	Temperature      float32    `json:"temperature"`
+	TopP             int        `json:"top_p"`
+	FrequencyPenalty int        `json:"frequency_penalty"`
+	PresencePenalty  int        `json:"presence_penalty"`
+}
+
+type messages struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+// Completions gtp文本模型回复
+//curl https://api.openai.com/v1/completions
+//-H "Content-Type: application/json"
+//-H "Authorization: Bearer your chatGPT key"
+//-d '{"model": "text-davinci-003", "prompt": "give me good song", "temperature": 0, "max_tokens": 7}'
+func Completions(msg string) (string, error) {
+	requestBody := ChatGPTRequestBody{
+		Model:            "gpt-3.5-turbo",
+		Prompt:           []messages{{Role: "user", Content: msg}},
+		MaxTokens:        2048,
+		Temperature:      0.7,
+		TopP:             1,
+		FrequencyPenalty: 0,
+		PresencePenalty:  0,
+	}
+	requestData, err := json.Marshal(requestBody)
+
+	if err != nil {
+		return "", err
+	}
+	log.Printf("request gtp json string : %v", string(requestData))
+	req, err := http.NewRequest("POST", BASEURL+"completions", bytes.NewBuffer(requestData))
+	if err != nil {
+		return "", err
+	}
+
+	apiKey := config.LoadConfig().ApiKey
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	gptResponseBody := &ChatGPTResponseBody{}
+	log.Println(string(body))
+	err = json.Unmarshal(body, gptResponseBody)
+	if err != nil {
+		return "", err
+	}
+	var reply string
+	if len(gptResponseBody.Choices) > 0 {
+		for _, v := range gptResponseBody.Choices {
+			if v.Msg.Role == "assistant" {
+				reply = v.Msg.Content
+			}
+		}
+	}
+	log.Printf("gpt response text: %s \n", reply)
+	return reply, nil
+}
